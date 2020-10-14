bd56aa8c9dc482d75e0cbbcffdbfe30584d657d7
rogeriodelphi <rogeriodelphi@gmail.com>
2020-10-14 03:38:58 -0300

    Adicionado arquivo .travis.yml

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name='post_detail'),
 ]
