3a60c0fa6ce3fa8e3184ddfbd2dc463f91d84590
rogeriodelphi <rogeriodelphi@gmail.com>
2020-10-19 02:24:37 -0300

    Check with flake8

--- blog/views.py
+++ blog/views.py
@@ -62,10 +62,8 @@ def post_detail(request, year, month, day, post):
 
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
-        .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
-                        .order_by('-same_tags', '-publish')[:4]
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
     return render(request,
                   'blog/post/detail.html',
