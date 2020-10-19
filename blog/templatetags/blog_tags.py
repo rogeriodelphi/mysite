d37ca58cb880a9eb092a6922406e088155be1e93
rogeriodelphi <rogeriodelphi@gmail.com>
2020-10-19 13:38:09 -0300

    Creating custom template tags and filters

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,32 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+
+register = template.Library()
+
+from django.utils.safestring import mark_safe
+import markdown
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+
+# reate a custom filter to enable you to use markdown syntax in your blog
+# posts and then convert the post contents to HTML in the templates
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
