f9b169ba78451be32fabb147fcabe0154f091f66
rogeriodelphi <rogeriodelphi@gmail.com>
2020-10-20 23:43:47 -0300

    Adding full-text search to blog - Building a search view

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, \
     PageNotAnInteger
@@ -6,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 
@@ -104,3 +105,17 @@ def post_share(request, post_id):
         return render(request, 'blog/post/share.html', {'post': post,
                                                         'form': form,
                                                         'sent': sent})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
