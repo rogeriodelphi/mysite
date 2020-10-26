5274f48fa8b6abb45385ae0bda95caabee990ea3
rogeriodelphi <rogeriodelphi@gmail.com>
2020-10-25 23:48:26 -0300

    Building a search view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, \
@@ -116,9 +117,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = Post.published.annotate(
-                similarity=TrigramSimilarity('title', query),
-            ).filter(similarity__gt=0.1).order_by('-similarity')
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
+
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
