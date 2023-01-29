8a80f4ef0c775e3b8e8aeb7add70738fdc71bf40
rogeriodelphi <rogeriodelphi@gmail.com>
2023-01-29 15:30:33 -0300

    Atualizando os arquivos de configuração

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '*0#(j=7!&0m*%&$0&w2u6ju7$ugbip71d-&)-9zcnsd#oc#*w3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mysite-production-592c.up.railway.app', '127.0.0.1', 'localhost']
 
